b81957b5166930d5ab9b51761d17b5d9885d0c75
benjcarter <benjcarter06@gmail.com>
2026-04-07 00:58:13 -0300

    initial setup, shadcn init, and theming

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
